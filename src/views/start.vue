3c4520750ebd52dbaa2d4b3bd40d571b991745dd
marahmanjs <imarahmandev@gmail.com>
2022-08-19 00:12:15 +0500

    tailwind integration and cleanup
    Installed tailwind css for styling purposes and cleanup unwanted files generated using vue cli create

---
+++ src/views/start.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="start">
+    <h1>Start Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'StartView',
+});
+</script>
